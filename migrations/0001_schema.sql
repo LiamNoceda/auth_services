4f34697f12877a571f7345d76030ac53a7343780
Orion Stellaris <jonhfeel88@gmail.com>
2026-08-24 23:22:31 +0400

    Add IF NOT EXISTS clause to users table creation

--- migrations/0001_schema.sql
+++ migrations/0001_schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id BIGINT PRIMARY KEY DEFAULT ('x' || lpad(encode(gen_random_bytes(7), 'hex'), 16, '0'))::bit(64)::bigint,
     username VARCHAR(50) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL
